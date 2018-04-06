1756b8c612966ec54a260f8789932d47f39d7069
pinchia0420 <30792829+pinchia0420@users.noreply.github.com>
2018-04-06 02:47:40 -0400

    upload resume.js

---
+++ myport/js/resume.js
@@ -0,0 +1,49 @@
+// project javascript
+$(document).ready(function(){
+  $(".preview").on("mouseover focus", showInfo);
+  $(".preview").on("mouseleave blur", hideInfo);
+});
+function showInfo(){
+  if (this.id === "internImg") {
+    //console.log("internImg")
+    $("#internText").css("display", "flex");
+  }
+  else if (this.id === "paraImg") {
+    //console.log("paraImg")
+    $("#paraText").css("display", "flex");
+  }
+  else if (this.id === "IRImg") {
+    //console.log("IRImg")
+    $("#IRText").css("display", "flex");
+  }
+  else if (this.id === "MLImg") {
+    //console.log("MLImg")
+    $("#MLText").css("display", "flex");
+  }
+  else {
+    //console.log("CVImg")
+    $("#CVText").css("display", "flex");
+  }
+}
+function hideInfo(){
+  if (this.id === "internImg") {
+    //console.log("internImg")
+    $("#internText").css("display", "none");
+  }
+  else if (this.id === "paraImg") {
+    //console.log("paraImg")
+    $("#paraText").css("display", "none");
+  }
+  else if (this.id === "IRImg") {
+    //console.log("IRImg")
+    $("#IRText").css("display", "none");
+  }
+  else if (this.id === "MLImg") {
+    //console.log("MLImg")
+    $("#MLText").css("display", "none");
+  }
+  else {
+    //console.log("CVImg")
+    $("#CVText").css("display", "none");
+  }
+}
